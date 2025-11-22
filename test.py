df765f03f0802defd9ec10c2cb5bdce793d22cff
Cal Cu <whiteblanksheet@gmail.com>
2025-11-22 12:31:31 -0800

    Add dice roll loop with y/n prompt

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import random
 
-print(random.randint(1, 6))
+def roll():
+    return random.randint(1, 6)
+
+while True:
+    print(roll())
+    choice = input("Roll again? (y/n): ").strip().lower()
+    if choice in ("y", "yes"):
+        continue
+    if choice in ("n", "no"):
+        break
